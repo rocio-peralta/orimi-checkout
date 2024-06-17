5ba57145f1585f88557cd2382e05a4ec344fe93e
Rocio-Peralta <1rocioperalta@gmail.com>
2024-06-18 07:10:13 +1200

    added user

--- orimi-checkout/src/index.ts
+++ orimi-checkout/src/index.ts
@@ -7,19 +7,20 @@ type Env = {
   API_SECRET_STRIPE: string
 }
 
+type Body = {
+  cart: Cart[]
+  user: User
+}
+
 const app = new Hono<{ Bindings: Env }>()
-const YOUR_DOMAIN = 'http://localhost:5174'
+
 app.use('*', cors())
-app.get('/', (c) => {
-  return c.json({ message: 'Hello World' })
-})
 
 app.post('/create-checkout-session', async (c) => {
-  const body: Cart[] = await c.req.json()
-  const User: User = await c.req.json()
+  const body: Body = await c.req.json()
 
-  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = body.map(
-    (product) => {
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] =
+    body.cart.map((product) => {
       return {
         price_data: {
           currency: 'usd',
@@ -32,17 +33,16 @@ app.post('/create-checkout-session', async (c) => {
         },
         quantity: product.quantity,
       }
-    }
-  )
+    })
 
   const stripeInstance = new stripe(c.env.API_SECRET_STRIPE)
 
   const session = await stripeInstance.checkout.sessions.create({
-    // ui_mode: 'embedded',
-    // line_items,
-    // mode: 'payment',
-    // return_url: `${YOUR_DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
-    // customer_email: 'rocio@gmail.com',
+    ui_mode: 'embedded',
+    line_items,
+    mode: 'payment',
+    return_url: 'http://localhost:5173/about',
+    customer_email: body.user.email,
   })
 
   return c.json({ clientSecret: session.client_secret })
