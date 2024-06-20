f08ea6e156afee0498910c1fdcd1d2f6db62378f
Rocio-Peralta <1rocioperalta@gmail.com>
2024-06-21 06:14:23 +1200

    add port

--- orimi-checkout/src/index.ts
+++ orimi-checkout/src/index.ts
@@ -16,63 +16,9 @@ const app = new Hono<{ Bindings: Env }>()
 
 app.use('*', cors())
 
-app.get('/profile/orders', async (c) => {
-  const stripeInstance = new stripe(c.env.API_SECRET_STRIPE, {
-    apiVersion: '2024-04-10',
-  })
+const LOCAL_DOMAIN = 'http://localhost:5173'
 
-  try {
-    const orders = await stripeInstance.checkout.sessions.list({
-      limit: 3,
-    })
-
-    return c.json(orders.data)
-  } catch (error) {
-    console.error('Error fetching orders:', error)
-    return c.json({ error: 'Failed to fetch orders' }, 500)
-  }
-})
-
-// Nueva ruta para obtener los detalles de un pedido específico
-app.get('/profile/orders/:orderId', async (c) => {
-  const { orderId } = c.req.param()
-  const stripeInstance = new stripe(c.env.API_SECRET_STRIPE, {
-    apiVersion: '2024-04-10',
-  })
-
-  try {
-    const session = await stripeInstance.checkout.sessions.retrieve(orderId, {
-      expand: ['line_items.data.price.product'],
-    })
-
-    if (!session || !session.line_items) {
-      return c.json({ error: 'Order not found' }, 404)
-    }
-
-    // Estructura de datos del pedido
-    const order = {
-      id: session.id,
-      created: session.created,
-      amount_total: session.amount_total,
-      currency: session.currency,
-      items: session.line_items.data.map((item) => {
-        const product = item.price?.product as Stripe.Product | undefined
-        return {
-          id: item.id,
-          name: item.description,
-          quantity: item.quantity,
-          price: item.amount_total,
-          image1: product?.images[0] ?? '',
-        }
-      }),
-    }
-
-    return c.json(order)
-  } catch (error) {
-    console.error('Error fetching order:', error)
-    return c.json({ error: 'Failed to fetch order' }, 500)
-  }
-})
+const DOMAIN = 'https://orimi-develop.vercel.app'
 
 app.post('/create-checkout-session', async (c) => {
   const body: Body = await c.req.json()
@@ -99,11 +45,80 @@ app.post('/create-checkout-session', async (c) => {
     ui_mode: 'embedded',
     line_items,
     mode: 'payment',
-    return_url: 'http://localhost:5173/paymentsuccesfull',
+    return_url: `${LOCAL_DOMAIN}/paymentsuccessfull?session_id={CHECKOUT_SESSION_ID}`,
     customer_email: body.user.email,
   })
 
   return c.json({ clientSecret: session.client_secret })
 })
 
+app.get('/session-status', async (c) => {
+  const stripeInstance = new stripe(c.env.API_SECRET_STRIPE)
+  // return c.json({ session: c.req.query('session_id') })
+
+  const session = await stripeInstance.checkout.sessions.retrieve(
+    c.req.query('session_id') as string
+  )
+  console.log('session: ', session)
+  return c.json({ session})
+})
+
 export default app
+
+// app.get('/profile/orders', async (c) => {
+//   // const body: Body = await c.req.json()
+//   const stripeInstance = new stripe(c.env.API_SECRET_STRIPE, {
+//     apiVersion: '2024-04-10',
+//   })
+
+//   try {
+//     const orders = await stripeInstance.checkout.sessions.list({
+//       limit: 3,
+//     })
+//     return c.json(orders.data)
+//   } catch (error) {
+//     console.error('Error fetching orders:', error)
+//     return c.json({ error: 'Failed to fetch orders' }, 500)
+//   }
+// })
+
+// // Nueva ruta para obtener los detalles de un pedido específico
+// app.get('/profile/orders/:orderId', async (c) => {
+//   const { orderId } = c.req.param()
+//   const stripeInstance = new stripe(c.env.API_SECRET_STRIPE, {
+//     apiVersion: '2024-04-10',
+//   })
+
+//   try {
+//     const session = await stripeInstance.checkout.sessions.retrieve(orderId, {
+//       expand: ['line_items.data.price.product'],
+//     })
+
+//     if (!session || !session.line_items) {
+//       return c.json({ error: 'Order not found' }, 404)
+//     }
+
+//     // Estructura de datos del pedido
+//     const order = {
+//       id: session.id,
+//       created: session.created,
+//       amount_total: session.amount_total,
+//       currency: session.currency,
+//       items: session.line_items.data.map((item) => {
+//         const product = item.price?.product as Stripe.Product | undefined
+//         return {
+//           id: item.id,
+//           name: item.description,
+//           quantity: item.quantity,
+//           price: item.amount_total,
+//           image1: product?.images[0] ?? '',
+//         }
+//       }),
+//     }
+
+//     return c.json(order)
+//   } catch (error) {
+//     console.error('Error fetching order:', error)
+//     return c.json({ error: 'Failed to fetch order' }, 500)
+//   }
+// })
