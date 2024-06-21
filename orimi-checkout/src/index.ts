8756d861aa04a0cf60f091c30ac4de390faacccc
Rocio-Peralta <1rocioperalta@gmail.com>
2024-06-22 07:06:15 +1200

    deleted commented

--- orimi-checkout/src/index.ts
+++ orimi-checkout/src/index.ts
@@ -20,6 +20,10 @@ const LOCAL_DOMAIN = 'http://localhost:5173'
 
 const DOMAIN = 'https://orimi-develop.vercel.app'
 
+app.get('/', (c) => {
+  return c.json({ message: 'Hello from Orimi' })
+})
+
 app.post('/create-checkout-session', async (c) => {
   const body: Body = await c.req.json()
 
@@ -54,71 +58,11 @@ app.post('/create-checkout-session', async (c) => {
 
 app.get('/session-status', async (c) => {
   const stripeInstance = new stripe(c.env.API_SECRET_STRIPE)
-  // return c.json({ session: c.req.query('session_id') })
-
   const session = await stripeInstance.checkout.sessions.retrieve(
     c.req.query('session_id') as string
   )
   console.log('session: ', session)
-  return c.json({ session})
+  return c.json({ session })
 })
 
 export default app
-
-// app.get('/profile/orders', async (c) => {
-//   // const body: Body = await c.req.json()
-//   const stripeInstance = new stripe(c.env.API_SECRET_STRIPE, {
-//     apiVersion: '2024-04-10',
-//   })
-
-//   try {
-//     const orders = await stripeInstance.checkout.sessions.list({
-//       limit: 3,
-//     })
-//     return c.json(orders.data)
-//   } catch (error) {
-//     console.error('Error fetching orders:', error)
-//     return c.json({ error: 'Failed to fetch orders' }, 500)
-//   }
-// })
-
-// // Nueva ruta para obtener los detalles de un pedido específico
-// app.get('/profile/orders/:orderId', async (c) => {
-//   const { orderId } = c.req.param()
-//   const stripeInstance = new stripe(c.env.API_SECRET_STRIPE, {
-//     apiVersion: '2024-04-10',
-//   })
-
-//   try {
-//     const session = await stripeInstance.checkout.sessions.retrieve(orderId, {
-//       expand: ['line_items.data.price.product'],
-//     })
-
-//     if (!session || !session.line_items) {
-//       return c.json({ error: 'Order not found' }, 404)
-//     }
-
-//     // Estructura de datos del pedido
-//     const order = {
-//       id: session.id,
-//       created: session.created,
-//       amount_total: session.amount_total,
-//       currency: session.currency,
-//       items: session.line_items.data.map((item) => {
-//         const product = item.price?.product as Stripe.Product | undefined
-//         return {
-//           id: item.id,
-//           name: item.description,
-//           quantity: item.quantity,
-//           price: item.amount_total,
-//           image1: product?.images[0] ?? '',
-//         }
-//       }),
-//     }
-
-//     return c.json(order)
-//   } catch (error) {
-//     console.error('Error fetching order:', error)
-//     return c.json({ error: 'Failed to fetch order' }, 500)
-//   }
-// })
