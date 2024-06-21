1c28225959a16239d6115780d0382684f23006cd
Pedro Vega <pedrovegadamian@gmail.com>
2024-06-21 16:10:24 -0500

    aaa

--- orimi-checkout/src/index.ts
+++ orimi-checkout/src/index.ts
@@ -1,37 +1,34 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-import stripe, { type Stripe } from 'stripe'
-import { Product as Cart } from './models/productModel'
-import { UserPrimaryData as User } from './models/userModel'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import stripe, { type Stripe } from "stripe";
+import { Product as Cart } from "./models/productModel";
+import { UserPrimaryData as User } from "./models/userModel";
 type Env = {
-  API_SECRET_STRIPE: string
-}
+  DOMAIN_URL: string;
+  API_SECRET_STRIPE: string;
+};
 
 type Body = {
-  cart: Cart[]
-  user: User
-}
+  cart: Cart[];
+  user: User;
+};
 
-const app = new Hono<{ Bindings: Env }>()
+const app = new Hono<{ Bindings: Env }>();
 
-app.use('*', cors())
+app.use("*", cors());
 
-const LOCAL_DOMAIN = 'http://localhost:5173'
+app.get("/", (c) => {
+  return c.json({ message: c.env });
+});
 
-const DOMAIN = 'https://orimi-develop.vercel.app'
-
-app.get('/', (c) => {
-  return c.json({ message: 'Hello from Orimi' })
-})
-
-app.post('/create-checkout-session', async (c) => {
-  const body: Body = await c.req.json()
+app.post("/create-checkout-session", async (c) => {
+  const body: Body = await c.req.json();
 
   const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] =
     body.cart.map((product) => {
       return {
         price_data: {
-          currency: 'usd',
+          currency: "usd",
           product_data: {
             name: product.name,
             description: product.description,
@@ -40,29 +37,29 @@ app.post('/create-checkout-session', async (c) => {
           unit_amount: product.price * 100,
         },
         quantity: product.quantity,
-      }
-    })
+      };
+    });
 
-  const stripeInstance = new stripe(c.env.API_SECRET_STRIPE)
+  const stripeInstance = new stripe(c.env.API_SECRET_STRIPE);
 
   const session = await stripeInstance.checkout.sessions.create({
-    ui_mode: 'embedded',
+    ui_mode: "embedded",
     line_items,
-    mode: 'payment',
-    return_url: `${DOMAIN}/paymentsuccessfull?session_id={CHECKOUT_SESSION_ID}`,
+    mode: "payment",
+    return_url: `${c.env.DOMAIN_URL}/paymentsuccessfull?session_id={CHECKOUT_SESSION_ID}`,
     customer_email: body.user.email,
-  })
+  });
 
-  return c.json({ clientSecret: session.client_secret })
-})
+  return c.json({ clientSecret: session.client_secret });
+});
 
-app.get('/session-status', async (c) => {
-  const stripeInstance = new stripe(c.env.API_SECRET_STRIPE)
+app.get("/session-status", async (c) => {
+  const stripeInstance = new stripe(c.env.API_SECRET_STRIPE);
   const session = await stripeInstance.checkout.sessions.retrieve(
-    c.req.query('session_id') as string
-  )
-  console.log('session: ', session)
-  return c.json({ session })
-})
+    c.req.query("session_id") as string
+  );
+  console.log("session: ", session);
+  return c.json({ session });
+});
 
-export default app
+export default app;
